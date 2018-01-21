67e89244766a8a0d33cfa6a245fb4a5ca75c2842
kylesallette <kylesallette@gmail.com>
2018-01-21 16:53:48 -0700

    all finished

--- app/controllers/task_manager_app.rb
+++ app/controllers/task_manager_app.rb
@@ -1,12 +1,15 @@
+require_relative '../models/task.rb'
+
 class TaskManagerApp < Sinatra::Base
   set :root, File.expand_path("..", __dir__)
+  set :method_override, true
 
   get '/' do
     erb :dashboard
   end
 
   get '/tasks' do
-    @tasks = ["task1", "task2", "task3"]
+    @tasks = Task.all
     erb :index
   end
 
@@ -15,6 +18,28 @@ class TaskManagerApp < Sinatra::Base
   end
 
   post '/tasks' do
-    "<p>Params: #{params}</p> <p>Task params: #{params[:task]}</p>"
+    task = Task.new(params[:task])
+    task.save
+    redirect '/tasks'
+  end
+
+  get '/tasks/:id' do
+    @task = Task.find(params[:id])
+    erb :show
+  end
+
+  get '/tasks/:id/edit' do
+    @task = Task.find(params[:id])
+    erb :edit
+  end
+
+  put '/tasks/:id' do |id|
+    Task.update(id.to_i, params[:task])
+    redirect "/tasks/#{id}"
+  end
+
+  delete '/tasks/:id' do |id|
+    Task.destroy(id.to_i)
+    redirect '/tasks'
   end
 end
